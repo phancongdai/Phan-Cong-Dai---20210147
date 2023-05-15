908760058888699552b24334a994c8b661aeacc5
phancongdai <116451170+phancongdai@users.noreply.github.com>
2023-05-15 17:44:50 +0700

    Update GarbageCreator.java

--- OtherProject/src/hust/soict/globalict/garbage/GarbageCreator.java
+++ OtherProject/src/hust/soict/globalict/garbage/GarbageCreator.java
@@ -2,16 +2,19 @@ package hust.soict.globalict.garbage;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
 public class GarbageCreator {
     public static void main(String[] args) throws IOException {
-        String filename = "test.exe";
+        String filename = "OtherProjects/src/hust/soict/globalict/garbage/big.txt";
         byte[] inputBytes = { 0 };
-        long startTime , endTime ;
+        long startTime, endTime;
+
         inputBytes = Files.readAllBytes(Paths.get(filename));
         startTime = System.currentTimeMillis();
-        StringBuilder outputStringBuilder = new StringBuilder();
-        for(byte b : inputBytes)
-            outputStringBuilder.append((char)b);
+        String outputString = "";
+        for (byte b : inputBytes) {
+            outputString += (char) b;
+        }
         endTime = System.currentTimeMillis();
         System.out.println(endTime - startTime);
     }
