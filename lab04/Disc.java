dd5574fc503fcb03272af03e590a4e51204306c1
phancongdai <116451170+phancongdai@users.noreply.github.com>
2023-06-11 21:58:41 +0700

    lab04 commit

---
+++ lab04/Disc.java
@@ -0,0 +1,30 @@
+package hust.soict.dsai.media;
+
+public class Disc extends Media {
+    private int length;
+    private String director;
+    public int getLength() {
+        return length;
+    }
+    public String getDirector() {
+        return director;
+    }
+    public Disc(String title, String category, float cost, int length, String director) {
+        super(title, category, cost);
+        this.length = length;
+        this.director = director;
+    }
+    public Disc(String title) {
+        super(title);
+
+    }
+    public Disc(String title, String category, float cost) {
+        super(title, category, cost);
+
+    }
+    public Disc(String title, String category, float cost, String director) {
+        super(title, category, cost);
+        this.director = director;
+
+    }
+}
